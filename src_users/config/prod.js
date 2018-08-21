0a3910892be3af658e2a8e75ae77235e85390d1f
anthonychung14 <anthonychung14@gmail.com>
2018-08-20 22:14:05 -0700

    firebase update

---
+++ src_users/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyBAw9WJ5SZ9PgEA5I-HYlJuMxFgIN14bNc",
+  authDomain: "saga-two-you.firebaseapp.com",
+  databaseURL: "https://saga-two-you.firebaseio.com",
+  projectId: "saga-two-you",
+  storageBucket: "saga-two-you.appspot.com",
+  messagingSenderId: "232833390558"
+};
