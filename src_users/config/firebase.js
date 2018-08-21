62e98b27f6cd949ab01e8accdd494711884003ff
anthonychung14 <anthonychung14@gmail.com>
2018-08-20 22:28:18 -0700

    Revert "more base"
    This reverts commit a963083d53018f3b41876b7461704a218434dbad.

--- src_users/config/firebase.js
+++ src_users/config/firebase.js
@@ -1,15 +1,7 @@
-import { database, auth } from "firebase";
+import * as firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyBAw9WJ5SZ9PgEA5I-HYlJuMxFgIN14bNc",
-  authDomain: "saga-two-you.firebaseapp.com",
-  databaseURL: "https://saga-two-you.firebaseio.com",
-  projectId: "saga-two-you",
-  storageBucket: "saga-two-you.appspot.com",
-  messagingSenderId: "232833390558"
-};
-
-firebase.initializeApp(config);
+import { FirebaseConfig } from "./keys";
+firebase.initializeApp(FirebaseConfig);
 
 const databaseRef = firebase.database().ref();
 export const todosRef = databaseRef.child("questions");
