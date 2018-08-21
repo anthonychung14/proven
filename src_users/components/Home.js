1de1cd48058413dc6b29545bc996d511c5aec144
anthonychung14 <anthonychung14@gmail.com>
2018-08-21 07:50:52 -0700

    more

--- src_users/components/Home.js
+++ src_users/components/Home.js
@@ -4,15 +4,12 @@ import CurrencyRequestList from "./common/CurrencyRequestList";
 
 // Home page component
 
-const RequestReport = () => {};
-
 export default class Home extends React.Component {
   // render
   render() {
     return (
       <div className="page-home">
         <CurrencyRequestList />
-        <RequestReport />
       </div>
     );
   }
