a963083d53018f3b41876b7461704a218434dbad
anthonychung14 <anthonychung14@gmail.com>
2018-08-20 22:21:06 -0700

    more base

--- src_users/config/firebase.js
+++ src_users/config/firebase.js
@@ -1,7 +1,15 @@
-import * as firebase from "firebase";
+import { database, auth } from "firebase";
 
-import { FirebaseConfig } from "./keys";
-firebase.initializeApp(FirebaseConfig);
+const config = {
+  apiKey: "AIzaSyBAw9WJ5SZ9PgEA5I-HYlJuMxFgIN14bNc",
+  authDomain: "saga-two-you.firebaseapp.com",
+  databaseURL: "https://saga-two-you.firebaseio.com",
+  projectId: "saga-two-you",
+  storageBucket: "saga-two-you.appspot.com",
+  messagingSenderId: "232833390558"
+};
+
+firebase.initializeApp(config);
 
 const databaseRef = firebase.database().ref();
 export const todosRef = databaseRef.child("questions");
