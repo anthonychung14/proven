98f257c8a8947a1199476bba6bfb9d864bea9622
anthonychung14 <anthonychung14@gmail.com>
2018-08-20 22:11:58 -0700

    put that back

--- src_users/components/Home.js
+++ src_users/components/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import CurrencyRequestList from "./common/CurrencyRequestList";
 
 // Home page component
+
+const RequestReport = () => {};
+
 export default class Home extends React.Component {
   // render
   render() {
     return (
       <div className="page-home">
         <CurrencyRequestList />
+        <RequestReport />
       </div>
     );
   }
